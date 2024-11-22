99f71c3cb8651fb136a9e57c7284cf4634fa1d6a
ivanvarella <ivanvarella@gmail.com>
2024-11-22 11:39:14 -0300

    Delete old files

--- process_analysis/old/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Process
-
-class ProcessForm(ModelForm):
-    class Meta:
-        model = Process
-        fields = ['arquivo']
--- process_analysis/old/urls_old.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("upload/", views.upload_process, name="upload_process"),
-    path("", views.process_list, name="process_list"),
-]
